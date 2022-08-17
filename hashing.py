1b4da6769c8eacdbdb2821fc420930d82a06f2a6
Yeganegharibi <104130850+Yeganegharibi@users.noreply.github.com>
2022-08-17 23:15:24 +0430

    Add files via upload

---
+++ hashing.py
@@ -0,0 +1,245 @@
+import csv
+import threading
+import hashlib
+
+
+ramz = dict()
+adad_hash = dict()
+with open('password.csv') as csvfile :
+    spamreader = csv.reader(csvfile)
+    for name, password in spamreader :
+        ramz[password] = name
+
+def hash_num1(n) :
+    while n != 625 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string1 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string1
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+
+def hash_num2 (n) :
+    while n != 1250 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string2 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string2
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+
+def hash_num3 (n) :
+    while n != 1875 :
+        # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string3 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string3
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num4 (n) :
+    while n != 2500 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string4 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string4
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num5 (n) :
+    while n != 3125 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string5 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string5
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num6 (n) :
+    while n != 3750 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string6 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string6
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num7 (n) :
+    while n != 4375 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string7 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string7
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num8 (n) :
+    while n != 5000 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string8 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string8
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num9 (n) :
+    while n != 5625 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string9 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string9
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num10 (n) :
+    while n != 6250 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string10 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string10
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num11 (n) :
+    while n != 6875 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string11 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string11
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num12 (n) :
+    while n != 7500 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string12 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string12
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num13 (n) :
+    while n != 8125 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string13 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string13
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num14 (n) :
+    while n != 8750 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string14 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string14
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num15 (n) :
+    while n != 9375 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string15 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string15
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+def hash_num16 (n) :
+    while n != 10000 :
+    # Hash a single string with hashlib.sha256
+        a_string = str(n)
+        hashed_string16 = hashlib.sha256(a_string.encode('utf-8')).hexdigest()
+        # print(hashed_string)
+        adad_hash[str(n)] = hashed_string16
+        if adad_hash[str(n)] == ramz :
+            print('password %s hast %s ' %(name, str(n)) )
+        n += 1
+
+if __name__ == "__main__" :
+    t1 = threading.Thread(target= hash_num1, args=(1,)) 
+    t2 = threading.Thread(target= hash_num2, args=(625,))
+    t3 = threading.Thread(target= hash_num3, args=(1250,)) 
+    t4 = threading.Thread(target= hash_num4, args=(1875,))
+    t5 = threading.Thread(target= hash_num5, args=(2500,)) 
+    t6 = threading.Thread(target= hash_num6, args=(3125,))
+    t7 = threading.Thread(target= hash_num7, args=(3750,)) 
+    t8 = threading.Thread(target= hash_num8, args=(4375,))
+    t9 = threading.Thread(target= hash_num9, args=(5000,)) 
+    t10 = threading.Thread(target= hash_num10, args=(5625,))
+    t11 = threading.Thread(target= hash_num11, args=(6250,)) 
+    t12 = threading.Thread(target= hash_num12, args=(6875,))
+    t13 = threading.Thread(target= hash_num13, args=(7500,)) 
+    t14 = threading.Thread(target= hash_num14, args=(8125,))
+    t15 = threading.Thread(target= hash_num15, args=(8750,)) 
+    t16 = threading.Thread(target= hash_num16, args=(9375,))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+    t8.start()
+    t9.start()
+    t10.start()
+    t11.start()
+    t12.start()
+    t13.start()
+    t14.start()
+    t15.start()
+    t16.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    t6.join()
+    t7.join()
+    t8.join()
+    t9.join()
+    t10.join()
+    t11.join()
+    t12.join()
+    t13.join()
+    t14.join()
+    t15.join()
+    t16.join()
+
+
+
+
